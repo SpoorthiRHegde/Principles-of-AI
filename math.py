13c8b4018442e764143871828403dfa9ff645156
Spoorthi R Hegde <151832654+SpoorthiRHegde@users.noreply.github.com>
2024-05-30 20:58:24 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,29 @@
+#built in function
+x=min(3,56,34,1)
+print(x)
+y=(45,56,76,23)
+print(max(y))
+
+z=(-2.58)
+print(z)
+
+print(pow(4,3))
+
+#math module
+import math
+a=math.sqrt(95)
+print(a)
+
+print(math.ceil(1.4))
+print(math.floor(1.4))
+
+b=math.pi
+print(b)
+
+print(math.cos(45))
+
+print(math.factorial(5))
+
+print(math.gcd(2,5))
+
+print(math.log(10))
