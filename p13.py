f93d497b660b55f87fd3519db3163585babc7565
Spoorthi R Hegde <151832654+SpoorthiRHegde@users.noreply.github.com>
2024-06-14 22:53:03 +0530

    Add files via upload

---
+++ p13.py
@@ -0,0 +1,7 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Iterate through the list and print each number multiplied by 2
+for num in numbers:
+    multiplied = num * 2
+    print(multiplied)
